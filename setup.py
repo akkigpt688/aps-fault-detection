2beb07e9e0ae0bd2e82af383be3e5ecf5226ca73
akkigpt688 <akkigpt688@gmail.com>
2023-01-29 12:14:22 +0530

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages,setup
 from typing import List
 
 REQUIREMENT_FILE_NAME = "requirements.txt"
-
+HYPHEN_E_DOT = "-e ."
 def get_requirements()-> List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
